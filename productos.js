c1ff46c995036e02c32961e627bf826e3bd6e334
Sotoenzo <enzonatanaelsoto@gmail.com>
2025-10-27 19:56:37 -0300

    Agregue un botonModal.classlist en js

--- productos.js
+++ productos.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const botonModal = document.createComment("button");
     botonModal.textContent = "Mostrarme el mensaje";
+    botonModal.classlist.add("btn", "animar");
     
 })
