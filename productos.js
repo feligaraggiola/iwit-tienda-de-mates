4764e370e5b78cc47a6f9ee0392f8e252025e8ba
Sotoenzo <enzonatanaelsoto@gmail.com>
2025-10-27 19:57:37 -0300

    Agregue un botonModal.style.display en js

--- productos.js
+++ productos.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const botonModal = document.createComment("button");
     botonModal.textContent = "Mostrarme el mensaje";
     botonModal.classlist.add("btn", "animar");
-    
+    botonModal.style.display = "block";
 })
