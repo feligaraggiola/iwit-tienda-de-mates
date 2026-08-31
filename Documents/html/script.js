a16347a59c55741475cfc0ae5f6fc5748cd62665
Enzo Soto <enzonatanaelsoto@gmail.com>
2026-08-31 09:45:19 -0300

    acomode el modal de script.js

--- Documents/html/script.js
+++ Documents/html/script.js
@@ -12,21 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const seccionTestimonios = document.getElementById("testimonios");
   seccionTestimonios.parentNode.insertBefore(botonModal, seccionTestimonios);
   
-    // === Crear el modal (oculto al inicio) ===
-    const modal = document.createElement("div");
-    modal.classList.add("mostrar"); // animación de aparición
-    Object.assign(modal.style, {
-      position: "fixed",
-      top: "0",
-      left: "0",
-      width: "100%",
-      height: "100%",
-      display: "none",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "rgba(0,0,0,0.5)",
-      zIndex: "1000"
-    });
+    
+  const modal = document.createElement("div");
+  modal.classList.add("mostrar"); // animación de aparición
+  Object.assign(modal.style, {
+    position: "fixed",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    display: "none",
+    justifyContent: "center",
+    alignItems: "center",
+    background: "rgba(0,0,0,0.5)",
+    zIndex: "1000"
+  });
   
     // === Contenido del modal ===
     const modalContenido = document.createElement("div");
